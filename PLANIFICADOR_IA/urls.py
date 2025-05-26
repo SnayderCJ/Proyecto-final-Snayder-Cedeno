0fdc232eeafe7327c3090f09e5254b8ef74d3d29
fborjaz <fborjaz@unemi.edu.ec>
2025-05-26 16:40:04 -0500

    Actualiza la configuración de URLs para la gestión de cuentas, reemplazando la inclusión de 'allauth.urls' por 'accounts.urls'.

--- PLANIFICADOR_IA/urls.py
+++ PLANIFICADOR_IA/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls', namespace='core')), 
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
